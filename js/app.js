6660b3a42ed4c7787e73bebb5b1dd5d621bbebf6
WaleedHaji <waleed4haji@gmail.com>
2026-08-03 19:10:36 +0300

    feat: added starter code set up and constant values and variables

---
+++ js/app.js
@@ -0,0 +1,30 @@
+/*-------------------------------- Constants --------------------------------*/
+const valueX = ('X')
+const valueO = ('O')
+
+console.log(valueO, valueX)
+
+/*---------------------------- Variables (state) ----------------------------*/
+let sqrZero = null
+let sqrOne = null
+let sqrTwo = null
+let sqrThree = null
+let sqrFour = null
+let sqrFive = null
+let sqrSix = null
+let sqrSeven = null
+let sqrEight = null
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
