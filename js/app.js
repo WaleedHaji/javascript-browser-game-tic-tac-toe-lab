a7c4e81a4aecfc2335c364219c542a433cbd3ff7
WaleedHaji <waleed4haji@gmail.com>
2026-08-05 20:38:18 +0300

    feat: & bug: added const squareIndex, funtions placePiece and switchTurn and fixed bugs

--- js/app.js
+++ js/app.js
@@ -1,27 +1,28 @@
 /*-------------------------------- Constants --------------------------------*/
-const valueX = ('X')
-const valueO = ('O')
+// const valueX = ('X')
+// const valueO = ('O')
 
 const winningCombos = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 4, 8],
-  [1, 4, 7],
-  [2, 4, 6],
-  [0, 3, 6],
-  [2, 5, 8],
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 4, 6],
+    [0, 3, 6],
+    [2, 5, 8],
 ]
+
+// const squareIndex = 
 
 // console.log(valueO, valueX)
 
 /*---------------------------- Variables (state) ----------------------------*/
-let board = ['X', '', '', '', '', '','', '', ''];
+let board = ['', '', '', '', '', '', '', '', ''];
 
 let turn = 'X'
 
 let playerO = 'O'
-
 let winner = false
 
 let tie = false
@@ -34,55 +35,80 @@ const squareEls = document.querySelectorAll('.sqr')
 const messageElements = document.querySelector('#message')
 
 console.log(squareEls)
-console.log(messageElements)
+console.log(messageElements.textContent)
 
 /*-------------------------------- Functions --------------------------------*/
-function init(){
+function init() {
     console.log('game start')
+    render()
 
 }
 
-init()
-
-function render(){
-    function updateBoard(){
-    board.forEach(function(oneSquare, index){
-        squareEls[index].textContent = oneSquare
-        console.log(oneSquare)
-    })
-
+function render() {
 
     updateBoard()
 
-    function updateMessage(){
-     if(winner === false && tie === false){
+    updateMessage()
+    // messageElements.innerHTML = message
+}
+
+
+init()
+function updateMessage() {
+    if (winner === false && tie === false) {
         console.log(`'Player' ${turn} 'turn'`)
-        message = `Player ${turn} 'turn'`;
-     }
-
-     else if(winner === false && tie === true){
-        console.log('Tie Game')
-        message = "Tie Game"
-        }
-
-     else {
-        console.log(`'Player' ${turn}  'wins'`)
-        }
+        console.log(messageElements)
+        messageElements.textContent = `Player ${turn} turn`;
     }
 
-    messageElements.innerHTML = message
+    else if (winner === false && tie === true) {
+        console.log('Tie Game')
+        messageElements.textContent = "Tie Game"
+    }
 
+    else {
+        console.log(`'Player' ${turn}  'wins'`)
+        messageElements.textContent = `'Player' ${turn} 'wins'`
+    }
 }
+function updateBoard() {
+    board.forEach(function (oneSquare, index) {
+        squareEls[index].textContent = oneSquare
+        console.log(oneSquare)
+    })
+}
+
+function placePiece(index){
+    board[index] = turn
+}
+
+function switchTurn(){
+    if(turn === 'X'){
+        turn = 'O'
+    }
+    else{
+        turn = 'X'
+    }
+}
+function handleClick(event) {
+    squareIndex = event.target.id
+    if(board[squareIndex] || winner){ return}
+    placePiece(squareIndex)
+    console.log(event.target.id)
+
+    // checkForWinner()
+    // checkForTie()
+    switchTurn()
+    render()
 }
 
 
-// function clickedSquare (){
-//     console.log(event.target.id)
-// }
+
 
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 
-// for(let oneSquareElements of squareEls){
-//     oneSquareElements.addEventListener('click', clickedSquare)}
+for (let oneSquareElements of squareEls) {
+    oneSquareElements.addEventListener('click', handleClick)
+}
