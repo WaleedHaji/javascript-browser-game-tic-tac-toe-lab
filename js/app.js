3997df04d7b7123122e39931af3f79559d0b4c87
WaleedHaji <waleed4haji@gmail.com>
2026-08-05 19:30:55 +0300

    feat: added steps 4 and 5 from the exervise sheet

--- js/app.js
+++ js/app.js
@@ -1,15 +1,26 @@
 /*-------------------------------- Constants --------------------------------*/
-// const valueX = ('X')
-// const valueO = ('O')
+const valueX = ('X')
+const valueO = ('O')
+
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 4, 6],
+  [0, 3, 6],
+  [2, 5, 8],
+]
 
 // console.log(valueO, valueX)
 
 /*---------------------------- Variables (state) ----------------------------*/
-let board = ['', '', '',
-             '', '', '',
-             '', '', ''];
+let board = ['X', '', '', '', '', '','', '', ''];
 
 let turn = 'X'
+
+let playerO = 'O'
 
 let winner = false
 
@@ -28,40 +39,42 @@ console.log(messageElements)
 /*-------------------------------- Functions --------------------------------*/
 function init(){
     console.log('game start')
+
 }
 
 init()
 
 function render(){
-    
-}
-
-function updateBoard(){
+    function updateBoard(){
     board.forEach(function(oneSquare, index){
         squareEls[index].textContent = oneSquare
         console.log(oneSquare)
     })
-}
 
-updateBoard()
 
-function updateMessage(){
-    if(winner === false && tie === false){
+    updateBoard()
+
+    function updateMessage(){
+     if(winner === false && tie === false){
         console.log(`'Player' ${turn} 'turn'`)
         message = `Player ${turn} 'turn'`;
-    }
+     }
 
-    else if(winner === false && tie === true){
+     else if(winner === false && tie === true){
         console.log('Tie Game')
         message = "Tie Game"
+        }
+
+     else {
+        console.log(`'Player' ${turn}  'wins'`)
+        }
     }
 
-    else {
-        console.log(`'Player' ${turn}  'wins'`)
-    }
+    messageElements.innerHTML = message
+
+}
 }
 
-messageElements.innerHTML = message
 
 // function clickedSquare (){
 //     console.log(event.target.id)
@@ -72,7 +85,4 @@ messageElements.innerHTML = message
 
 
 // for(let oneSquareElements of squareEls){
-//     oneSquareElements.addEventListener('click', clickedSquare)
-// }
-
-
+//     oneSquareElements.addEventListener('click', clickedSquare)}
