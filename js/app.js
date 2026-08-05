e46ae0f6236bad6117fa6d14abd87a9704399324
WaleedHaji <waleed4haji@gmail.com>
2026-08-05 15:04:52 +0300

    feat: added function updateMessage and its conditions, along with a message variable

--- js/app.js
+++ js/app.js
@@ -5,13 +5,17 @@
 // console.log(valueO, valueX)
 
 /*---------------------------- Variables (state) ----------------------------*/
-let board = ['', '', '', '', '', '', '', '', ''];
+let board = ['', '', '',
+             '', '', '',
+             '', '', ''];
 
 let turn = 'X'
 
 let winner = false
 
 let tie = false
+
+let message = null
 
 /*------------------------ Cached Element References ------------------------*/
 const squareEls = document.querySelectorAll('.sqr')
@@ -26,12 +30,38 @@ function init(){
     console.log('game start')
 }
 
+init()
+
 function render(){
     
 }
 
-init()
+function updateBoard(){
+    board.forEach(function(oneSquare, index){
+        squareEls[index].textContent = oneSquare
+        console.log(oneSquare)
+    })
+}
 
+updateBoard()
+
+function updateMessage(){
+    if(winner === false && tie === false){
+        console.log(`'Player' ${turn} 'turn'`)
+        message = `Player ${turn} 'turn'`;
+    }
+
+    else if(winner === false && tie === true){
+        console.log('Tie Game')
+        message = "Tie Game"
+    }
+
+    else {
+        console.log(`'Player' ${turn}  'wins'`)
+    }
+}
+
+messageElements.innerHTML = message
 
 // function clickedSquare (){
 //     console.log(event.target.id)
