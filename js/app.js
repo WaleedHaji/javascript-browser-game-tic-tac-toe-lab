4dd8e361b68370107a9b3dada165bc0dc6371e73
WaleedHaji <waleed4haji@gmail.com>
2026-08-06 15:03:54 +0300

    feat:, bug:, refactor: added checkForWinner function, had alot of issues with it, but finally fixed it. Cleaned up the code by removing unwanted code/comments

--- js/app.js
+++ js/app.js
@@ -1,33 +1,19 @@
 /*-------------------------------- Constants --------------------------------*/
-// const valueX = ('X')
-// const valueO = ('O')
 
-const winningCombos = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 4, 8],
-    [1, 4, 7],
-    [2, 4, 6],
-    [0, 3, 6],
-    [2, 5, 8],
+
+const winningCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [1, 4, 7], [2, 4, 6], [0, 3, 6], [2, 5, 8]
 ]
 
-// const squareIndex = 
-
-// console.log(valueO, valueX)
 
 /*---------------------------- Variables (state) ----------------------------*/
 let board = ['', '', '', '', '', '', '', '', ''];
 
 let turn = 'X'
 
-let playerO = 'O'
 let winner = false
 
 let tie = false
 
-let message = null
 
 /*------------------------ Cached Element References ------------------------*/
 const squareEls = document.querySelectorAll('.sqr')
@@ -49,11 +35,11 @@ function render() {
     updateBoard()
 
     updateMessage()
-    // messageElements.innerHTML = message
+   
 }
 
-
 init()
+
 function updateMessage() {
     if (winner === false && tie === false) {
         console.log(`'Player' ${turn} 'turn'`)
@@ -68,9 +54,10 @@ function updateMessage() {
 
     else {
         console.log(`'Player' ${turn}  'wins'`)
-        messageElements.textContent = `'Player' ${turn} 'wins'`
+        messageElements.textContent = `Player ${turn} wins`
     }
 }
+
 function updateBoard() {
     board.forEach(function (oneSquare, index) {
         squareEls[index].textContent = oneSquare
@@ -90,15 +77,31 @@ function switchTurn(){
         turn = 'X'
     }
 }
+
+function checkForWinner(){
+    for (let oneWinner of winningCombos){
+       let [a, b, c] = oneWinner;
+        if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        console.log("Winner found!");
+        winner = true
+        return 
+        }
+    }
+
+
+}
+
+
 function handleClick(event) {
-    squareIndex = event.target.id
+    const squareIndex = event.target.id
     if(board[squareIndex] || winner){ return}
     placePiece(squareIndex)
     console.log(event.target.id)
 
-    // checkForWinner()
+    checkForWinner()
     // checkForTie()
-    switchTurn()
+    if (!winner){
+    switchTurn()}
     render()
 }
 
